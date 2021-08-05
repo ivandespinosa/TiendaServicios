1361bbf16d5abc28629864a05d910b6897a761bb
Iván Espinosa <iespinosa@grupoafin.com>
2021-08-05 11:17:43 -0500

    Agregar archivos de proyecto.

---
+++ TiendaServicios.Api.Libro.Tests/LibrosServiceTest.cs
@@ -0,0 +1,136 @@
+﻿using AutoMapper;
+using GenFu;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TiendaServicios.Api.Libro.Aplicacion;
+using TiendaServicios.Api.Libro.Modelo;
+using TiendaServicios.Api.Libro.Persistencia;
+using Xunit;
+
+namespace TiendaServicios.Api.Libro.Tests
+{
+    public class LibrosServiceTest
+    {
+        private IEnumerable<LibreriaMaterial> ObtenerDataPrueba()
+        {
+            A.Configure<LibreriaMaterial>()
+                .Fill(x => x.Titulo).AsArticleTitle()
+                .Fill(x => x.LibreriaMaterialId, () => { return Guid.NewGuid(); });
+
+            var lista = A.ListOf<LibreriaMaterial>(30);
+            lista[0].LibreriaMaterialId = Guid.Empty;
+
+            return lista;
+        }
+
+        private Mock<ContextoLibreria> CrearContexto()
+        {
+            var dataPrueba = ObtenerDataPrueba().AsQueryable();
+
+            /*
+             Con las siguientes líneas le estamos indicando a entity framework que la clase LibreriaMaterial tiene que ser una clase de tipo entidad
+             para conseguir esto debemos de darle la configuración de Setup, darle un Provider, darle un Expression, darle un ElementType y darle
+             un GetEnumerator; éstas son las propiedades que debe tener todas clase de Entity Framework; al no estar trabajando con una 
+             configuración directa de Sql Server o una instancia de persistencia debemos hacer manualmente este trabajo.
+             */
+            var dbSet = new Mock<DbSet<LibreriaMaterial>>();
+            dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x => x.Provider).Returns(dataPrueba.Provider);
+            dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x => x.Expression).Returns(dataPrueba.Expression);
+            dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x => x.ElementType).Returns(dataPrueba.ElementType);
+            dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x => x.GetEnumerator()).Returns(dataPrueba.GetEnumerator());
+
+            dbSet.As<IAsyncEnumerable<LibreriaMaterial>>().Setup(x => x.GetAsyncEnumerator(new System.Threading.CancellationToken()))
+                .Returns(new AsyncEnumerator<LibreriaMaterial>(dataPrueba.GetEnumerator()));
+
+            dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x => x.Provider).Returns(new AsyncQueryProvider<LibreriaMaterial>(dataPrueba.Provider));
+
+            var contexto = new Mock<ContextoLibreria>();
+            contexto.Setup(x => x.LibreriaMaterial).Returns(dbSet.Object);
+            return contexto;            
+        }
+
+        [Fact]
+        public async void GetLibroPOrId()
+        {
+            var mockContexto = CrearContexto();
+            var mapConfig = new MapperConfiguration(cfg =>
+            {
+               cfg.AddProfile(new MappingTest());
+            });
+
+            var mapper = mapConfig.CreateMapper();
+
+            var request = new ConsultaFiltro.LibroUnico();
+            request.LibroId = Guid.Empty;
+
+            var manejador = new ConsultaFiltro.Manejador(mockContexto.Object, mapper);
+
+            var libro = await manejador.Handle(request, new System.Threading.CancellationToken());
+
+            Assert.NotNull(libro);
+            Assert.True(libro.LibreriaMaterialId == Guid.Empty);
+        }
+
+        [Fact]
+        public async void GetLibros()
+        {
+            System.Diagnostics.Debugger.Launch();
+
+            // Que método dentro de mi microservice libro se está encargando
+            // de realizar la consulta de libros de la base de datos???
+
+            // 1. Emular a la instancia de entity framework core - ContextoLibreria
+            // Para emular las acciones y eventos de un objeto en un ambiente de unit test
+            // utilizamos objetos de tipo mock
+
+            var mockContexto = CrearContexto();
+
+            // 2. Emular al mapping IMapper
+
+            var mapConfig = new MapperConfiguration(cfg =>
+            {
+               cfg.AddProfile(new MappingTest());
+            });
+
+            var mapper = mapConfig.CreateMapper();
+
+            // 3. Instanciar a la clase Manejador y pasarle como parametros los mocks que he creado
+
+            Consulta.Manejador manejador = new Consulta.Manejador(mockContexto.Object, mapper);
+
+            Consulta.Ejecuta request = new Consulta.Ejecuta();
+
+            var lista = await manejador.Handle(request, new System.Threading.CancellationToken());
+
+            Assert.True(lista.Any());
+        }
+
+        [Fact]
+        public async void GuardarLibro()
+        {
+            System.Diagnostics.Debugger.Launch();
+
+            var options = new DbContextOptionsBuilder<ContextoLibreria>()
+                .UseInMemoryDatabase(databaseName: "BaseDatosLibro")
+                .Options;
+
+            var contexto = new ContextoLibreria(options);
+
+            var request = new Nuevo.Ejecuta();
+            request.Titulo = "Libro de Microservice";
+            request.AutorLibro = Guid.Empty;
+            request.FechaPublicacion = DateTime.Now;
+
+            var manejador = new Nuevo.Manejador(contexto);
+
+            var libro = await manejador.Handle(request, new System.Threading.CancellationToken());
+
+            Assert.True( libro != null );
+
+        }
+    }
+}
